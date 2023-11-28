94005775bf147957c802140f6e459e08af4b7159
siyandaMaphitha <siyasi19@gmail.com>
2023-11-28 13:03:01 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-"""Print the alphabet in lower, not followed by a new line."""
-
-for letter in range(97,123):
+letter in range(97,123):
     print("{}".format(chr(letter)), end="")
