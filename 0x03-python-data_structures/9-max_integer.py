46b626fb777eb7b22d71ddb37794cd0eac83095e
siyandaMaphitha <siyasi19@gmail.com>
2023-12-05 09:11:31 -0800

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/bin/bash/python3
+def max_interger(my_list=[]):
+    if len(my_list) == 0:
+        return "None"
+    else:
+        max = my_list[0]
+        for in range(len(my_list)):
+            if my_list[i] > max"
+            max = my_list[i]
+            return max
